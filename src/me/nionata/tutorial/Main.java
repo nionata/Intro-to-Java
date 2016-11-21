72e5f18f28b8f8f42955e1d7f5df90ae226bb13c
Nicholas Ionata <nicktheskill@gmail.com>
2016-11-21 09:48:52 -0500

    Episode 3

--- src/me/nionata/tutorial/Main.java
+++ src/me/nionata/tutorial/Main.java
@@ -3,36 +3,5 @@ package me.nionata.tutorial;
 public class Main {
 	
 	public static void main(String[] args) {
-		/*
-		 * x = 5
-		 * y = 3.5
-		 */
-	
-		//Whole Numbers 1, -5, 10
-		int whole;
-		whole = 5;
-		
-		System.out.println(whole);
-		
-		whole = 6;
-		
-		System.out.println(whole);
-		
-		int whole2 = 5;
-	
-		//Rational numbers 6.12, 3.14, 12.789
-		double ratio;
-		ratio = 3.14;
-		
-		double ratio2 = 5.5;
-	
-		//Words and sentences "Sup peeps"
-		String name = "nick";
-	
-		//Characters 'n'
-		char n = 'n';
-	
-		//True or False 
-		boolean truth = false;
 	}
 }
